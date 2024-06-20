cd78d9295bef26212e1195d4151e606745ee29c8
Jéssica Caroline <jessicacaroline3213@hotmail.com>
2024-06-20 00:30:41 -0300

    First commit of project

---
+++ src/main/java/br/jessicaoliveira/rest/core/BaseTestes.java
@@ -0,0 +1,32 @@
+package br.jessicaoliveira.rest.core;
+
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import org.hamcrest.Matchers;
+import org.junit.BeforeClass;
+
+import java.util.regex.Matcher;
+
+public class BaseTestes implements Constantes {
+
+    @BeforeClass
+    public static void setup(){
+        RestAssured.baseURI = APP_BASE_URL;
+        RestAssured.port = APP_PORT;
+        RestAssured.basePath = APP_BASE_PATH;
+
+        // Configura o tipo de conteúdo das requisições
+        RequestSpecBuilder reqBuilder = new RequestSpecBuilder();
+        reqBuilder.setContentType(APP_CONTENT_TYPE);
+        RestAssured.requestSpecification = reqBuilder.build();
+
+        // Configura o tempo máximo de espera para as respostas
+        ResponseSpecBuilder resBuilder = new ResponseSpecBuilder();
+        resBuilder.expectResponseTime(Matchers.lessThan(MAX_TIMEOUT));
+        RestAssured.responseSpecification = resBuilder.build();
+
+        // Habilita o logging das requisições e respostas apenas se a validação falhar
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+    }
+}
